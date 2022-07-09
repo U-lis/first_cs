efe426378aadeb08d2598e89328a2d413a716611
Kevin Moon <ulismoon@gmail.com>
2022-07-10 01:41:11 +0900

    First OOP
    Create Book class

---
+++ chapter4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Book gulliver = new Book();
+        }
+    }
+
+    class Book
+    {
+        string Title;
+        decimal ISBN13;
+        string Contents;
+        string Author;
+        int PageCount;
+    }
+}
