51b30505a9539347a8ab1a944adbc49ea1f37edc
Kevin Moon <ulismoon@gmail.com>
2022-07-11 14:56:55 +0900

    Use virtual and override keyword to work as expected

--- chapter4/ConsoleApp1/ConsoleApp1/Mammal.cs
+++ chapter4/ConsoleApp1/ConsoleApp1/Mammal.cs
@@ -8,24 +8,24 @@ namespace ConsoleApp1
 {
     internal class Mammal
     {
-        public void Move() {
+        virtual public void Move() {
             Console.WriteLine("Mammal Move");
         }
     }
 
     internal class Lion : Mammal {
-        public void Move() {
+        override public void Move() {
             Console.WriteLine("Move with four legs");
         }
     }
     internal class Whale : Mammal { 
-        public void Move() {
+        override public void Move() {
             Console.WriteLine("Swim!");
         }
     
     }
     internal class Human: Mammal { 
-        public void Move() {
+        override public void Move() {
             Console.WriteLine("Move with two legs");
         }
     }
